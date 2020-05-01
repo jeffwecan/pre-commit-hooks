da34ccef462c67ecb7a7c15d778a2e8ac3dd3c68
Jeffrey Hogan <jhogan@hashicorp.com>
2020-05-01 13:20:24 -0500

    addtl checks

---
+++ pre_commit_hooks/check_terraform.py
@@ -0,0 +1,22 @@
+import argparse
+import subprocess
+from typing import Any, Generator, NamedTuple, Optional, Sequence
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*', help='Filenames to check.')
+    args = parser.parse_args(argv)
+
+    retval = 0
+    for filename in args.filenames:
+        try:
+            subprocess.check_call(('terraform', '-check', '-diff', filename))
+        except subprocess.CalledProcessError as exc:
+            print(exc)
+            retval = 1
+    return retval
+
+
+if __name__ == '__main__':
+    exit(main())
